55a7325b769dc47095ed1b42fb4ea121628fc64f
Cody Reandeau <codyreandeau@yahoo.com>
2016-10-06 15:15:53 -0400

    Added error handling to the peek, pop, and push methods. Also created a peek method, and a method that returns the number of items in the stack.

--- Main.java
+++ Main.java
@@ -8,10 +8,18 @@ public class Main{
     arrayStack.push(5);
     arrayStack.push(6);
     arrayStack.push(9);
+    arrayStack.push(3);
+    arrayStack.push(7);
     
+    System.out.println("There are " + arrayStack.size() + " items in the stack");
+    System.out.println("The item on the top of the stack is: " + arrayStack.peek());
+    
+    System.out.print("The items in the list are: ");
     while(!arrayStack.isEmpty()){
       System.out.print(arrayStack.pop() + " ");
     }
     
+    System.out.println();
+    System.out.println("There are now " + arrayStack.size() + " items in the stack");
   }
 }
--- Stack.java
+++ Stack.java
@@ -1,13 +1,16 @@
+import java.util.*;
+
 public class Stack {
   
   private int[] array;
-  private int stackSize;
+  private int arraySize;
   private int topOfStack;
+  private int itemsInStack;
   
   Stack(int size) {
-    this.stackSize = size;
+    this.arraySize = size;
     this.topOfStack = 0;
-    this.array = new int[size];
+    this.array = new int[arraySize];
     }
   
   public boolean isEmpty() {
@@ -16,15 +19,35 @@ public class Stack {
   
   public void push(int x){
     
+    if (topOfStack == array.length){
+      throw new StackOverflowError();
+    }else{
     array[topOfStack] = x;
     topOfStack++;
-    stackSize++;
+    itemsInStack++;
+    }
 }
   
   public int pop(){
     
+    if(isEmpty()) {
+      throw new EmptyStackException();
+    }else{
     topOfStack--;
+    itemsInStack--;
     return array[topOfStack];
-    
+    }
+  }
+  
+  public int peek() {
+    if(isEmpty()) {
+      throw new EmptyStackException();
+    } else {
+    return array[topOfStack - 1];
+    }
+  }
+  
+  public int size() {
+    return itemsInStack;
   }
 }
